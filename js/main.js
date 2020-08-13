9951c1d607b7bbc735f87d4d9702b9002faacda3
Charles Muriuki <[muriukicharles54@gmail.com]>
2020-08-13 20:25:04 +0300

    js carousel  fixes- should kinda work now i hope

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 let navigation = document.querySelector('#navigation'),
-    productCards = document.querySelectorAll('.products_on_sale_card'),
-    carouselPreviousButton = document.querySelector('.previous_control'),
-    carouselNextButton = document.querySelector('.next_control'),
-    productsCarousel = document.querySelector('.product_on_sale_cards')
+
+    productCards = document.querySelectorAll('.product_on_sale_cards'),
+    productCards1 = document.querySelectorAll('.product_on_sale_cards_1 .products_on_sale_card'),
+    productCards2 = document.querySelectorAll('.product_on_sale_cards_2 .products_on_sale_card'),
+    productCards3 = document.querySelectorAll('.product_on_sale_cards_3 .products_on_sale_card'),
+
+    carouselPreviousButton1 = document.querySelector('.carousel_1_previous_control'),
+    carouselNextButton1 = document.querySelector('.carousel_1_next_control'),
+
+    carouselPreviousButton2 = document.querySelector('.carousel_2_previous_control'),
+    carouselNextButton2 = document.querySelector('.carousel_2_next_control'),
+
+    carouselPreviousButton3 = document.querySelector('.carousel_3_previous_control'),
+    carouselNextButton3 = document.querySelector('.carousel_3_next_control'),
+
+    productsCarousel1 = document.querySelector('.product_on_sale_cards_1'),
+    productsCarousel2 = document.querySelector('.product_on_sale_cards_2'),
+    productsCarousel3 = document.querySelector('.product_on_sale_cards_3')
+
 
 // ADD DARK BACKGROUND TO MENU WHEN SCROLLED
 window.addEventListener('scroll', function(e) {
@@ -32,23 +47,50 @@ productCards.forEach((card, index) => {
 
 // TRANSLATE THE CAROUSEL ON THE X AXIS
 
-//set the width of the parent container
-
+//set the width of the parent carousel container
 window.addEventListener('load', function() {
-    productsCarousel.style.width = `${(productCards.length) * 250}px`
+    productsCarousel1.style.width = `${(productCards1.length) * 250}px`
+    productsCarousel2.style.width = `${(productCards2.length) * 250}px`
+    productsCarousel3.style.width = `${(productCards3.length) * 250}px`
 })
 
-let startIndex = 0
+// start here ( add + 1 if clicking next, subtract - 1 to go go back)
+let startIndex1 = 0,
+    startIndex2 = 0,
+    startIndex3 = 0
 
-carouselPreviousButton.addEventListener('click', function() {
-    if(startIndex === 0) return
-    startIndex--
-    console.log(startIndex)
-    productsCarousel.style.transform = `translateX(${startIndex * 250}px)`
+// carousel 1
+carouselPreviousButton1.addEventListener('click', function() {
+    if(startIndex1 === 0) return
+    startIndex1--
+    productsCarousel1.style.transform = `translateX(${-startIndex1 * 250}px)`
 })
 
-carouselNextButton.addEventListener('click', function() {
-    startIndex++
-    console.log(startIndex)
-    productsCarousel.style.transform = `translateX(${-startIndex * 250}px)`
+carouselNextButton1.addEventListener('click', function() {
+    startIndex1++
+    productsCarousel1.style.transform = `translateX(${-startIndex1 * 250}px)`
+})
+
+// carousel 2
+carouselPreviousButton2.addEventListener('click', function() {
+    if(startIndex2 === 0) return
+    startIndex2--
+    productsCarousel2.style.transform = `translateX(${-startIndex2 * 250}px)`
+})
+
+carouselNextButton2.addEventListener('click', function() {
+    startIndex2++
+    productsCarousel2.style.transform = `translateX(${-startIndex2 * 250}px)`
+})
+
+// carousel 3
+carouselPreviousButton3.addEventListener('click', function() {
+    if(startIndex3 === 0) return
+    startIndex3--
+    productsCarousel3.style.transform = `translateX(${-startIndex3 * 250}px)`
+})
+
+carouselNextButton3.addEventListener('click', function() {
+    startIndex3++
+    productsCarousel3.style.transform = `translateX(${-startIndex3 * 250}px)`
 })
